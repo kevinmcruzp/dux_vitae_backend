84a3c94f6b868b97269fac9d5fcd718b692dbb2e
Kevin Cruz <kevin_mcp@outlook.com>
2022-07-07 11:19:00 -0400

    refactor(prisma): agregando columnas en la tabla de cliente

---
+++ prisma/migrations/20220707014718_agregando_column_as_en_la_tabla_de_cliente/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "clients" ADD COLUMN "birthday" TEXT;
+ALTER TABLE "clients" ADD COLUMN "description" TEXT;
+ALTER TABLE "clients" ADD COLUMN "gender" TEXT;
+ALTER TABLE "clients" ADD COLUMN "phone" TEXT;
